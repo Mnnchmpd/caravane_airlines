1b2865d43a85526e620f8ddd1ecb25f659e1c8d2
Manon Champaud <champaud.manon@gmail.com>
2019-05-23 17:54:15 +0200

    models, devise, controller

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'sassc-rails'
 gem 'simple_form'
 gem 'uglifier'
 gem 'webpacker'
+gem 'devise'
 
 group :development do
   gem 'web-console', '>= 3.3.0'
@@ -27,3 +28,4 @@ group :development, :test do
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'dotenv-rails'
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  devise_for :users
+  root to: 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+    resources :products do
+      resources :bookings, only: [:new, :create]
+    end
 end
