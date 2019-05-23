f937d08bd5a834da94496a508134399df5fafc38
camille lambert <lambertcamille23@gmail.com>
2019-05-23 18:57:49 +0200

    done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,45 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+   @product = Product.new(product_params)
+    if @product.save
+    redirect_to products_path(@products)
+    else
+      render :new
+    end
+  end
+
+    def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(params[:product])
+    @product.save
+    # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to product_path(@product)
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :address)
+  end
 end
