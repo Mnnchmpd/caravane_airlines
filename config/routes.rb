e83700a263dd854b1ab21e85890680b27d9f5aa8
Manon Champaud <champaud.manon@gmail.com>
2019-05-29 10:53:09 +0200

    test update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'contacts/index'
   devise_for :users
   root to: 'products#index'
   get "dashboard", to: "pages#dashboard"
+  get "home", to: "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     resources :products, only: [:index, :edit, :update, :show, :new, :create, :destroy] do
       resources :bookings, only: [:new, :create, :show, :index]
